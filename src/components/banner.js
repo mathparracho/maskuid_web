caef590dde44ffa5c7cbc87b6236698cce249884
Matheus Parracho <mathparracho@gmail.com>
2023-08-24 01:47:50 -0300

    ajuste posicao anuncio 2

--- src/components/banner.js
+++ src/components/banner.js
@@ -29,7 +29,8 @@ export default function Banner() {
 
     const containerStyle = {
         display: 'flex',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: '10px'
     };
 
     return (
